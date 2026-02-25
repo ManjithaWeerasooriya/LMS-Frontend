1deca25ecbb0c7e4b5d53295399652162753fa7f
Manjitha Weerasooriya <manjithawaa@gmail.com>
2026-02-25 15:26:28 +0530

    Create frontend scaffolding

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+// global.d.ts
+// Provide module declarations for CSS imports so TypeScript stops complaining
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
